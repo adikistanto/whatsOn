f2944227eba3606c331b9fd43361bbfff89ba7c2
Febra <m.febras@yahoo.com>
2015-07-01 23:38:51 +0700

    Penggunaan font google di header login - sign up Desain pengaturan

--- application/views/login.php
+++ application/views/login.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <title>What's On</title>
-<!--        <link href='http://fonts.googleapis.com/css?family=Gafata' rel='stylesheet' type='text/css'>-->
+        <link href='http://fonts.googleapis.com/css?family=Gafata' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" type="text/css" media="screen" />
         <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/whatsOn.css" type="text/css" media="screen" />
     </head>
